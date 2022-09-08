069bb2e47ee959efab1862d9aed34fce2f39eed4
ondieki1237 <makoriseth1237@gmail.com>
2022-09-08 12:22:32 -0700

    makori

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+/**
+ * main - print the string in the put function
+ *
+ * description: using the main function
+ * this program prints "Progaming is like building a multilingual puzzle
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
