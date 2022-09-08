4cf3a1d7019188174ccb04d5b69ae2007e4ed226
ondieki1237 <makoriseth1237@gmail.com>
2022-09-08 11:02:22 -0700

    makori

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #!/bim/bash
+/** this program is like building a multiple puzzle
+ * return 0 
+ * this file was written by seth
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
