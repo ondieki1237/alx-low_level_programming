915962a183d13467bf70086c8d45dcf33581cbc3
ondieki1237 <makoriseth1237@gmail.com>
2022-09-10 23:03:51 -0700

    makori

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * this project contains the first question
+ * more headers ges there
+ * betty style doc for function main goes there 
+ * return (0)
+ * */
+int main(void)
+{
+	int n;
+	strand(time(0));
+	n = rand() -RAND_max / 2;
+	/*this is the main code*/
+	return (0);
+}
