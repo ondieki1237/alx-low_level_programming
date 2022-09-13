8a8ace976551e8b30b82a5eb4604f3c5e0c7f6f9
ondieki1237 <makoriseth1237@gmail.com>
2022-09-12 21:54:56 -0700

    makori

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+#include<stdio.h>
+int main()
+{
+char ch = 'a';
+while (ch <= 'z');
+{
+putchar("%c", ch);
+ch++;
+}
+return 0;
+}
