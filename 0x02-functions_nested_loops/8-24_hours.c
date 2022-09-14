3aea3833c3e31b81565162812b35fce5809fb6c6
ondieki1237 <makoriseth1237@gmail.com>
2022-09-14 02:09:57 -0700

    makori

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
- * jack_bauer - a function that prints every minute of the day
- * @n: - from 00:00 to 23:59 and contains while loop counts
- * and resets the minutes
- * return (0)
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 void jack_bauer(void)
 {
 	int hours = 0;
 	int minutes = 0;
-	int hours_reminder = 0;
-	int mins_reminder = 0;
+	int hours_remainder;
+	int mins_remainder;
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59)
+	while (minutes <= 59)
 	{
-	mins_reminder = minutes %10;
-	hours_reminder = hours %10
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
 	_putchar(':');
@@ -25,9 +24,8 @@ void jack_bauer(void)
 	_putchar(mins_remainder + '0');
 	minutes++;
 	_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
 }
-hours++;
-minutes = 0;
-}
-}	
 }
