771845af8e7ef22fa998094ece86ce459b4fb54b
ondieki1237 <makoriseth1237@gmail.com>
2022-09-13 21:51:11 -0700

    makori

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
-#include "main.h'
-/**
+#include "main.h"
  * main - check the code.
  *
  * Return: Always 0.
@@ -8,7 +7,7 @@ void print_alphabets_x10(void)
 {
 char c, i;
 for (i = 0; i <= 9; i++)
-{
+{  
 for (c = 'a'; c<= 'z'; c++)
 {
 _putchar(c);
