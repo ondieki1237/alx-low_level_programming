5ebd6ef07cd1bd4acb888467a60551de5853a01e
ondieki1237 <makoriseth1237@gmail.com>
2022-09-14 00:01:19 -0700

    makori

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ *_abs - function that computes absolute value of an integer
+ *@c: is the int that will be used in the argument of the function
+ *return: 0
+ */
+int _abs(int)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else 
+	return (c * -1);
+}
