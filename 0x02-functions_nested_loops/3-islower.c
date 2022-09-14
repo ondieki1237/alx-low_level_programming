25a49837052b1eaf21aae7155691872565202057
ondieki1237 <makoriseth1237@gmail.com>
2022-09-13 22:15:27 -0700

    makori

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*
+* main - check the code.
+*_islower - function to check for lowercase character
+*
+* Return: Always 0.
+*/
+int _islower(int c)
+{
+if(ch >== 'a' && ch <= 'z')
+{
+return (1);
+}
+else 
+return (0);
+}
