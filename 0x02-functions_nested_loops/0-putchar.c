1d25d3512f2b7c28f0f9e7af38e16fc9628e04a9
ondieki1237 <makoriseth1237@gmail.com>
2022-09-13 21:13:41 -0700

    makori

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,29 +1,21 @@
-Task 0.txt
-Today
-3:13 PM
-
-Antony Bahati uploaded an item
-Text
-Task 0.txt
 #include "main.h"
-
 /**
- * main - print putchar
- * Return: always 0
- */
+*main-print putchar
+*return: always 0
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 
 }
