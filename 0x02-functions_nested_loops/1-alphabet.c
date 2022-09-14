ad6622d8c98f210a573c3ed92657ab8d8dd29d60
ondieki1237 <makoriseth1237@gmail.com>
2022-09-14 12:59:06 -0700

    makori

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-*print_alphabet - functio to print alphabets
-*
-*return type :0
-*/
-void print_alphabets(void)
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <='z'; c++)
-{
-_putchar(c);
-}
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 _putchar('\n');
 }
