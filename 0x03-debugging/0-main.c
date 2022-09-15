2011e2fd3eeb027a8ac35e51d5c7fa94aed162f3
ondieki1237 <makoriseth1237@gmail.com>
2022-09-15 04:25:01 -0700

    makori

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - test function that prints if integer if positive or negative
+ *return: 0
+ */
+
+int main (void)
+{
+    int i;
+    i = 0;
+    positive_or_negative(i);
+
+    return(0);
+}
