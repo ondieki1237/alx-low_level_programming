232623a58fdc546c6bb16d100eb9999bc5921df8
ondieki1237 <makoriseth1237@gmail.com>
2022-09-19 12:59:26 -0700

    makori

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "main.h"
 #include <string.h>
+#include "main.h"
 
 /**
 *print_rev - Prints a string in reverse order
@@ -11,7 +11,6 @@
 void print_rev(char *s)
 {
 	int len = strlen(s);
-
 	while (len--)
 		putchar(*(s + len));
 	putchar(10);
