98ad4d78699ef37d59f51fb3e37e475070a99a1b
ondieki1237 <makoriseth1237@gmail.com>
2022-09-19 11:38:50 -0700

    makori

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+/**
+*_strlen(char *s) - returns the length of the string
+*n@: - length of the string
+*Return: returns nothing
+*/
+
+int _strlen(char *s)
+{
+    int len = _strlen(C);
+    printf("Length = %d\n", len)
+}
