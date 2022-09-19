6358bfe30d231be51797f5ee54fb19a104982099
ondieki1237 <makoriseth1237@gmail.com>
2022-09-19 10:16:01 -0700

    makori

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
+void reset_to_98(int *n)
 
 /**
 *reset_to_98 takes a pointer to an int as parameter and updates
 *@n: - function
 *return: nothing
 */
-void reset_to_98(int *n)
 {
-*n = 98;
+    *n = 98;  
 }
