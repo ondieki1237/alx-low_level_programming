dadcb2b946c2087050d54cae47457826d99ce596
ondieki1237 <makoriseth1237@gmail.com>
2022-09-22 01:07:27 -0700

    makori

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,25 +8,11 @@
  *@src : to be concatenated
  *@n : number of bytes to not to be terminated
  *
- * Return: Always 0.
+ * Return: a new concatenated string
  */
-char *_strncat(char *dest, char *src, int n);
-{
-int index = 0;
-int dest_len =0;
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+char *_strncat(char *dest, char *src, int n)
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-	return (0);
+{
+    strncat(dest, src, n);
+    return (dest);
 }
