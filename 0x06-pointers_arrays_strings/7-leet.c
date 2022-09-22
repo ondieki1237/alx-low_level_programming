1aade031b8d4b180c881a95554bd3af3e0467e1b
ondieki1237 <makoriseth1237@gmail.com>
2022-09-22 13:10:36 -0700

    makori

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*leet(char *) - function for printing music numbers and letters
+*@s - the string that is to be encoded
+*
+*return (0)
+*/
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
+	}
+	return (s);
+}
