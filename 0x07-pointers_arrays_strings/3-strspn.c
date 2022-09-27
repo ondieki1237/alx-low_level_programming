aa8a61b6d3054233ba99614e925686741b6e9fff
ondieki1237 <makoriseth1237@gmail.com>
2022-09-26 19:09:05 -0700

    makori

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
+ *
+ * Return: the number of bytes in the initial segment
+ *
