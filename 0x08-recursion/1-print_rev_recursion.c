4bbd7bae4fadfc84ba2ed555eda473a416d6db2a
ondieki1237 <makoriseth1237@gmail.com>
2022-09-28 03:15:40 -0700

    makori

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
-*_print_rev_recursion: - a function that prints a string in reverse
-*@s: - the string to be printed in reverse
-*
-*return: always 0
-*/
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: the string to reverse
+ *
+ * Return: nothing
+ */
 
 void _print_rev_recursion(char *s)
 {
