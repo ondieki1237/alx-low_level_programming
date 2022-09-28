ae498bdda9fdfaa30c8a413c8ec3c117b8ef8ef7
ondieki1237 <makoriseth1237@gmail.com>
2022-09-27 19:05:18 -0700

    makori

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_string - function for checking the pointer
- *@to - the value to be changed to char
- *@s - value to be changed to char
- *
- *return: always 0
- */
+*set_string - function for checking the pointer
+*@to: - the value to be changed to changed
+*@s:- value to be changed to char
+*
+*return: always 0
+*/
 void set_string(char **s, char *to)
 {
 *s = to;
