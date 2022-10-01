022fbce000a622f638a857bdce317a400bef3665
ondieki1237 <makoriseth1237@gmail.com>
2022-10-01 13:05:44 -0700

    makori

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,16 @@
 #ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+#include <stdio.h>
+/**
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+
+#endif /*MAIN_H*/
