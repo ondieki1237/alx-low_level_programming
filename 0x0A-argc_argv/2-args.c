362567eeef2b3504674595ea0d9967295d55cadf
ondieki1237 <makoriseth1237@gmail.com>
2022-10-01 20:55:45 -0700

    makori

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argc[i]);
+}
+return (0);
+}
