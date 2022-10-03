9d058cbfb3ea6aa56b63c7755becf17a661a6b7e
ondieki1237 <makoriseth1237@gmail.com>
2022-10-02 20:01:57 -0700

    makori

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,32 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-/**
- * main - adds numbers
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: 0 if no errors, else 1
- */
 int main(int argc, char *argv[])
 {
-int a = 0, i, j;
-for (i = 1; i < argc; i++)
+int cents, coins = 0;
+
+if (argc != 2)
 {
-for (j = 0; argv[i][j]; j++)
+	printf("Error\n");
+	return (1);
+}
+cents = atoi(argv[1]);
+while (cents > 0)
 {
-if (isdigit(argv[i][j]) == 0)
-{
-puts("Error");
-return (1);
+	coins++;
+	if ((cents - 25) >= 0)
+	{
+		cents -= 25;
+		continue;
+	}
+	if ((cents - 10) >= 0)
+	{
+		cents -= 10;
+		continue;
+	}
+	if ((cents - 5) >= 0)
+	{
+		cents -= 5;
+		continue;
+	}
+	if ((cents - 2) >= 0)
+	{
+		cents -= 2;
+		continue;
+	}
+	cents--;
 }
-}
-}
-for (i = 1; i < argc; i++)
-{
-a += atoi(argv[i]);
-}
-printf("%d\n", a);
+printf("%d\n", coins);
 return (0);
 }
