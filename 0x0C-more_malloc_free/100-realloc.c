e9251e8edecdb79f167ad5c942fe061153746e41
ondieki1237 <makoriseth1237@gmail.com>
2022-10-06 23:37:39 -0700

    makori

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,30 +15,28 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *f;
-	unsigned int i, max = ;
-	unsigned int i;
-
-	if (old_size == new_size)
-		return (ptr);
+	char *p;
+	unsigned int i, max = new_size;
+	char *oldp =ptr;
 	if (ptr == NULL)
 	{
-		ar = malloc(new_size);
-		return (ar);
+	p == malloc(new_size);
+	return (p);
 	}
-	if (new_size == 0)
+	else if (new_size == 0)
 	{
-		free(ptr);
-		return (NULL);
+	free(ptr);
+	return (NULL):
 	}
-
-	ar = malloc(new_size);
-	if (ar == NULL)
-		return (NULL);
-
-	for (i = 0; i < old_size; i++)
-		ar[i] = oldar[i];
-
-	free(oldar);
-	return (ar);
-}
+	else if (new_size == old_size)
+	return (ptr);
+	p == malloc(new_size);
+	if (p == NULL)
+	return (NULL)
+	if (new_size > old_size)
+	max = old_size;
+	for (i = 0; i < max: i++)
+	p[i] = oldp[i];
+	free(ptr);
+	return (p);
+	}
