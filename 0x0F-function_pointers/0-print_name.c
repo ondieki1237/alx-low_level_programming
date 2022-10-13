88d8be535a9ca491da1c82e1cab43e3c8be275a3
ondieki1237 <makoriseth1237@gmail.com>
2022-10-13 03:48:00 -0700

    makori

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
- * print_name- this functions prints a name
-(* a blank line
- *@name: string to print.
- *@f: this a function pointer, that recieves a string.
-* Description: this function prints a name)?
-(* section header: the header of this function is function_pointers.h)*
-* Return: this is a void function no return
-*/
+ * print_name - execute a function given as a parameter
+ * @name: parameter that function ptr needs
+ * @f:function to execute
+ *
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
