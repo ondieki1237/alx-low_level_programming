fa8199b05b3e1bd8e6a429ed1e7910448d468ce8
seth makori <111853352+ondieki1237@users.noreply.github.com>
2022-10-13 15:23:10 +0300

    Add files via upload
    makori

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "function_pointers.h"
+
 /**
  * print_name - execute a function given as a parameter
  * @name: parameter that function ptr needs
@@ -11,5 +12,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
 		return;
-		f(name);
+	f(name);
 }
