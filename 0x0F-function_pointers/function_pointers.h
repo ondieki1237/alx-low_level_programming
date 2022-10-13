93d49e879255888a1eb2867dbad268426a409947
ondieki1237 <makoriseth1237@gmail.com>
2022-10-13 03:57:11 -0700

    makori

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,11 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTRS_H
+#define FUNCTION_POINTRS_H
 
+#include <stdio.h>
 #include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /*FUNCTION_POINTRS_H*/
