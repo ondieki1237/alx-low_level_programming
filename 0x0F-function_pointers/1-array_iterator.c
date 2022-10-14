5ffa7a90b4a021f23d3f35a26ad446549ab40138
ondieki1237 <makoriseth1237@gmail.com>
2022-10-13 19:39:26 -0700

    makori

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,1 @@
-#include <stdlib.h>
-#include "function_pointers.h"
-
-/**
- * array_iterator - a program that executes a func given as aparam
- * @size - the size of the array
- * @action - the pointer of the func used
- * @array - array to iterate over
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-unsigned int i;
-if (!array || !action)
-return;
-for (i = 0; i < size; i++)
-action(array[i]);
-}
+No arguments passed.
