796bbdc94bd877c2adef5c91d403f5d609954505
ondieki1237 <makoriseth1237@gmail.com>
2022-10-24 18:52:53 -0700

    makori

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,20 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* print_list - prints all elements of list_t list
+* list_len - prints all elements of list_t list
+* print_list - list of the printed
 * @h: linked list
 * Return: number of nodes in linked list
 */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 size_t count = 0;
 while (h)
 {
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
 count++;
 h = h->next;
 }
