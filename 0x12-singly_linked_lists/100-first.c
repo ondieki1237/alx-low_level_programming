edf06966f6a908e9df3c19c6f93a10af911f592c
ondieki1237 <makoriseth1237@gmail.com>
2022-10-24 19:56:36 -0700

    makori

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,2 +1,13 @@
- gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-first.c -o first
-ls
+#include <stdio.h>
+include "lists.h"
+
+/**
+* execute_before_main - executes this function before main function
+* Note: made possible by prior declaration of "__attribute__ ((constructor))"
+*/
+
+void __attribute__ ((constructor)) execute_before_main()
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
