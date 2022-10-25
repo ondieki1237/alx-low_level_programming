fa4b19a9e5427545b44fe4656529eb4fbbfdec7d
ondieki1237 <makoriseth1237@gmail.com>
2022-10-24 19:41:03 -0700

    makori

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,47 +1,31 @@
 #include "lists.h"
+#include <string.h>
 
 /**
-* _strlen - find string length
+* add_node - find string length
 * @str: string
+* @head: - a pointer at the head of the list
 * Return: length
 */
-int _strlen(const char *str)
-{
-int len;
-
-for (len = 0; str[len] != '\0'; len++)
-;
-return (len);
-}
-
-/**
-* add_node - add node to beginning of linked list
-* @head: linked list
-* @str: data for new node
-* Return: address of new element, or NULL if failed
-*/
-
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node; /* create new node */
-
-if (str == NULL) /* validate input */
+char *dup;
+int len;
+list_t *new
+new = malloc(sizeof(list_t));
+if (new == NULL)
 return (NULL);
-if (strdup(str) == NULL) /*check if strdup malloc errored */
+dup = strdup(str);
+if (dup == NULL)
+{
+free(new);
 return (NULL);
-
-new_node = malloc(sizeof(list_t)); /* malloc for new node */
-if (new_node == NULL)
-return (NULL);
-
-new_node->str = strdup(str); /* set node values */
-new_node->len = _strlen(str);
-
-if (head == NULL) /* set what new node points to first */
-new_node->next = NULL;
-else
-new_node->next = *head;
-
-*head = new_node; /* set head to point to new node */
-
-return (new_node);
+}
+for (len = 0; str[len];)
+len++;
+new->str = dup;
+new->len = len;
+new->next = *head;
+*head = new;
+return (new);
+}
