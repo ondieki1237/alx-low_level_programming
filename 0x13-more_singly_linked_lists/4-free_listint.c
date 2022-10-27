b18185129052210bf46d69b7fb00632f669a5947
ondieki1237 <bellarinseth@gmail.com>
2022-10-27 04:37:07 -0700

    makori

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* free_listint- frees a listint_t
+* @head: pointer to head pointer of linked list
+* Return: a freed linked list
+*/
+void free_listint(listint_t *head);
+{
+listint_t *tmp;
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
+}
