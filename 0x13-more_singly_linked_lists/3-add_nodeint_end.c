311be49ae3ec9f7d4b9ef872c0884a5539d85330
ondieki1237 <bellarinseth@gmail.com>
2022-10-27 05:04:10 -0700

    makori

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
 * add_nodeint_end - adds a new node at the end of a listint_t list
@@ -8,21 +9,23 @@
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new_node, *tmp;
-new_node = malloc(sizeof(listint_t)); /* make new node and set values */
-if (new_node == NULL)
-return (NULL);
+listint_t *new_node;
+listint_t *cursor = *head;
+new_node = malloc(sizeof(listint_t));
+if (new_node !== NULL)
+{
 new_node->n = n;
 new_node->next = NULL;
-
-if (*head == NULL) /* account for empty list by pointing head to node */
-*head = new_node;
-else
-{
-tmp = *head; /* set tmp ptr to iterate w/o moving head ptr */
-while (tmp->next != NULL) /* iterate till last node */
-tmp = tmp->next;
-tmp->next = new_node; /* link last node to new node */
 }
+else
+return (NULL);
+if (cursor != NULL)
+{
+while (cursor->next != NULL)
+cursor = cursor->next;
+cursor->next = new_node;
+}
+else
+*head = new_node;
 return (new_node);
 }
