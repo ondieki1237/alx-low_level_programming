d0c4e0c3241a339ffc3b74913a6c377fd61ea4bb
ondieki1237 <bellarinseth@gmail.com>
2022-10-27 19:47:39 -0700

    this one though

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,34 +1,53 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index - insert a new node at a given position
-* @head: pointer to head pointer of linked list
-* @idx: index to insert new node
-* @n: new node's data
-* Return: address of new node, or NULL if failed
-*/
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: pointer to head pointer of linked list
+ * @idx: index to insert new node
+ * @n: new node's data
+ * Return: address of new node, or NULL if failed
+ */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new, *temp, = *head;
-unsigned int node;
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-if (idx == 0)
-{
-new->next = temp;
-*head = new;
-return (new);
-}
-for (node = 0; node < (idx - 1); node++)
-{
-if (temp == NULL || temp->next == NULL)
-return (NULL);
-temp = temp->next;
-}
-new->next = temp->next;
-temp->next = new;
-return (new);
+
+	unsigned int i = 0;
+	listint_t *new_node, *tmp;
+
+	/* account for empty pointer */
+	if (head == NULL)
+		return (NULL);
+
+	/* malloc and set values for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	/* account for idx 0 */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* iterate to 1 before nth index to insert */
+	tmp = *head;
+	while (i < (idx - 1))
+	{
+		tmp = tmp->next;
+		i++;
+
+		if (tmp == NULL) /* account for idx out of range */
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+
+	/* link into list */
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
