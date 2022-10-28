aa0794c041efe6624290e4d304ca640cbbfe1f25
ondieki1237 <bellarinseth@gmail.com>
2022-10-27 19:58:05 -0700

    makori

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,98 +1,43 @@
-#include <stdio.h>
+
+
 #include "lists.h"
 
 /**
-* count_nodes_till_loop - count nodes to know now many unique nodes to print
-* @head: pointer to head pointer of linked list
-* Return: number of unique nodes in list before a loop
-*/
-int count_nodes_till_loop(const listint_t *head)
+ * free_listint_safe - frees a list (safe version)
+ * @h: linked list of type listint_t
+ *
+ * Return: number of nodes in freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
 {
-int count = 0;
-const listint_t *turtle, *hare;
+	listint_t *current, *runner, *head;
+	size_t h_i, r_i;
 
-turtle = hare = head;
+	if (h == NULL || *h == NULL)
+		return (0);
 
-while (turtle != NULL && hare != NULL)
-{
-turtle = turtle->next;
-hare = hare->next->next;
-count++;
+	current = *h;
+	head = *h;
+	h_i = 0;
 
-if (turtle == hare)
-{
-turtle = head;
-while (turtle != hare)
-{
-turtle = turtle->next;
-hare = hare->next;
-count++;
-}
-return (count);
-}
-}
-return (0);
-}
-
-/**
-* loop - find if there's a loop in linked list
-* @head: pointer to head pointer of linked list
-* Return: 0 if no loop, 1 if loop
-*/
-int loop(const listint_t *head)
-{
-const listint_t *turtle, *hare;
-
-turtle = hare = head;
-
-while (turtle != NULL && hare != NULL)
-{
-turtle = turtle->next;
-hare = hare->next->next;
-
-if (turtle == hare)
-return (1);
-}
-return (0);
-}
-
-/**
-* print_listint_safe - prints list with addresses
-* @head: pointer to head pointer of linked list
-* Return: number of nodes in list, exit(98) if failed
-*/
-size_t print_listint_safe(const listint_t *head)
-{
-int count = 0;
-int loop_found;
-size_t num_nodes = 0;
-const listint_t *tmp;
-
-if (head == NULL)
-exit(98);
-
-loop_found = loop(head);
-
-if (loop_found == 1) /* print upto last node before loop if loop */
-{
-count = count_nodes_till_loop(head);
-for (loop_found = 0; loop_found < count; loop_found++)
-{
-printf("[%p] %d\n", (void *)tmp, tmp->n);
-num_nodes += 1;
-tmp = tmp->next;
-}
-}
-else if (loop_found == 0) /* print regularly upto NULL if no loop */
-{
-tmp = head;
-while (tmp != NULL)
-{
-printf("[%p] %d\n", (void *)tmp, tmp->n);
-num_nodes += 1;
-tmp = tmp->next;
-}
-}
-
-return (num_nodes);
+	while (head != NULL)
+	{
+		runner = *h;
+		for (r_i = 0; r_i < h_i; r_i++)
+		{
+			if (runner == current)
+			{
+				*h = NULL;
+				return (h_i);
+			}
+			runner = runner->next;
+		}
+		current = head->next;
+		free(head);
+		head = current;
+		h_i++;
+	}
+	*h = NULL;
+	return (h_i);
 }
