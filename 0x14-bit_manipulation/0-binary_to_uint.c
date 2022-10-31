f772ca7978588285b6154b18c5e735e1445b328d
ondieki1237 <bellarinseth@gmail.com>
2022-10-31 04:25:29 -0700

    this one takes time

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
- */
+* binary_to_uint - convert binary to unsigned int
+* @b: binary
+* Return: unsigned int
+*/
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int val = 0;
-int i = 0;
-
+int len = 0, i;
+unsigned int sum = 0;
 if (b == NULL)
-return (0);
-while (b[i] == '0' || b[i] == '1')
+return (sum);
+while (b[len] != '\0')
+len++;
+len -= 1;
+while (b[i])
 {
-val <<= 1;
-val += b[i] - '0';
+if ((b[i] != '0') && (b[i] != '1'))
+return (sum);
+
+if (b[i] == '1')
+sum += (1 * (1 << len));
 i++;
+len--;
 }
-return (val);
+return (sum);
 }
