0da26b1643ce71b14fb5d13086c5f926388d68bd
ondieki1237 <bellarinseth@gmail.com>
2022-10-31 20:27:04 -0700

    corrected the trailling error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,8 @@ int len, base_two;
 if (!b)
 return (0);
 ui = 0;
-for (len = 0; b[len] != '\0'; len++);
+for (len = 0; b[len] != '\0'; len++)
+;
 for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
 if (b[len] != '0' && b[len] != '1')
