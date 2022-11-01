f4c6e7d7f4e8528e88cd6642c4fad19d7b877a22
ondieki1237 <bellarinseth@gmail.com>
2022-10-31 20:23:15 -0700

    the trailling statement error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,28 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* binary_to_uint - convert binary to unsigned int
-* @b: binary
-* Return: unsigned int
-*/
+ * binary_to_uint - convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int k = 1;
-unsigned int i = 0;
-int c;
-unsigned int len;
-len = strlen(b);
-for (c = len - 1; c >= 0; c--)
-{
-if (b[c] != '0' && b[c] != '1')
+unsigned int ui;
+int len, base_two;
+if (!b)
 return (0);
-if (b[c] == '1')
+ui = 0;
+for (len = 0; b[len] != '\0'; len++);
+for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
-i += k;
+if (b[len] != '0' && b[len] != '1')
+{
+return (0);
 }
-k *= 2;
+if (b[len] & 1)
+{
+ui += base_two;
 }
-return (i);
+}
+return (ui);
 }
