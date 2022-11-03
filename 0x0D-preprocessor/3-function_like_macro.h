90affcda1db8c8d7d1693c8b43b449c263553606
ondieki1237 <bellarinseth@gmail.com>
2022-11-02 23:54:20 -0700

    functioning like a pro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) (x >= 0 ? (x) : (-x))
-#endif
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
+
+#endif /* FUNCTION_LIKE_MACRO */
