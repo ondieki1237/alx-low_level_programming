5db7b6f05bd2c6f2601f3c28425464a2e5ab3595
seth makori <111853352+ondieki1237@users.noreply.github.com>
2024-05-22 09:22:37 +0300

    Update 4-print_rev.c
    changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,18 @@
-#include "main.h"
 /**
- * print_rev - prints a string, in reverse, followed by a new line.
- * @s: input string.
- * Return: no return.
+ * reverse_array - reverses an array
+ *
+ * @a: array to reverse
+ * @n: size of array
  */
-void print_rev(char *s)
+void reverse_array(int *a, int n)
 {
-	int count = 0;
+	int i;
+	int t;
 
-	while (count >= 0)
+	for (i = 0; i < n / 2; i++)
 	{
-		if (s[count] == '\0')
-			break;
-		count++;
+		t = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = t;
 	}
-
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
-	_putchar('\n');
 }
