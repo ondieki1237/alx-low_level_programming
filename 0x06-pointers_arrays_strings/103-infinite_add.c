166e6aed256790bc47e6c23d8621ac291917f6c0
ondieki1237 <makoriseth1237@gmail.com>
2024-08-31 19:01:40 -0700

    maks is back

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,49 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
- */
 
+/**
+ * infinite_add - Adds two numbers stored as strings.
+ * @n1: The first number.
+ * @n2: The second number.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ *
+ * Return: A pointer to the result (r), or 0 if the result can't be stored in r.
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+    int i, j, k, l, m, n;
+    int carry = 0;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
-	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
-	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+    /* Find the lengths of the strings n1 and n2 */
+    for (i = 0; n1[i]; i++);
+    for (j = 0; n2[j]; j++);
+    
+    /* If the result can't fit in r, return 0 */
+    if (i > size_r - 1 || j > size_r - 1)
+        return (0);
+
+    r[size_r] = '\0';  /* Null-terminate the buffer */
+    k = size_r - 1;
+
+    /* Add the numbers starting from the last digit */
+    for (i -= 1, j -= 1; i >= 0 || j >= 0 || carry; i--, j--, k--)
+    {
+        if (k < 0)
+            return (0);
+        m = (i >= 0) ? n1[i] - '0' : 0;
+        n = (j >= 0) ? n2[j] - '0' : 0;
+        r[k] = (m + n + carry) % 10 + '0';
+        carry = (m + n + carry) / 10;
+    }
+
+    if (k == size_r - 1)
+        return (0);
+
+    /* Shift the result to the start of the buffer if necessary */
+    for (l = 0, k++; k < size_r; l++, k++)
+        r[l] = r[k];
+    r[l] = '\0';
+
+    return (r);
 }
+
